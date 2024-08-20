295f99a2bcdcf59a5c02fa20373d2dcdf4d63f6d
jo-and75 <jaotika@gmail.com>
2024-08-20 22:26:01 +0300

    Finish inject method

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -2,12 +2,10 @@ module Enumerable
   def my_each_with_index  
     if block_given?   
       index = 0 
-      until index == self.length
-        self.my_each do |element|
+      self.my_each do |element|
           yield(element,index) 
           index += 1
-        end 
-      end
+      end 
     end 
     return self
   end 
@@ -53,7 +51,7 @@ module Enumerable
   def my_count  
     results = []
     if block_given? 
-      self.each do |element| 
+      self.my_each do |element| 
         results << element if yield(element) 
       end 
       return results.length  
@@ -65,12 +63,24 @@ module Enumerable
   def my_map 
     results = [] 
     if block_given? 
-      self.each do |element| 
+      self.my_each do |element| 
         results << yield(element) 
       end
     end 
     return results
+  end 
+
+  def my_inject(initial = nil)
+    accumulator = initial.nil? ? self.first : initial
+    start_index = initial.nil? ? 1 : 0
+    
+    self[start_index..-1].my_each_with_index do |element, index|
+      accumulator = yield(accumulator, element)
+    end    
+    accumulator
   end
+
+
   # Your code goes here
 end
 
